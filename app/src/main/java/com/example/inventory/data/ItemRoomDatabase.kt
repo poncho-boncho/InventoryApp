64611bbc882a0cd5c70e10156d7b41c79ced1497
poncho_boncho <Sorkolas@mail.ru>
2023-05-28 15:03:30 +0300

    created a database instance

--- app/src/main/java/com/example/inventory/data/ItemRoomDatabase.kt
+++ app/src/main/java/com/example/inventory/data/ItemRoomDatabase.kt
@@ -1,4 +1,29 @@
 package com.example.inventory.data
 
-class ItemRoomDatabase {
+import android.content.Context
+import androidx.lifecycle.ViewModelProvider.NewInstanceFactory.Companion.instance
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Item::class], version = 1, exportSchema = false)
+abstract class ItemRoomDatabase : RoomDatabase() {
+    abstract fun itemDao(): ItemDao
+    companion object {
+        @Volatile
+        private var INSTANCE: ItemRoomDatabase? = null
+        fun getDatabase(context: Context): ItemRoomDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ItemRoomDatabase::class.java,
+                    "item_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
